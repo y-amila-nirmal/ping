fdc0927a13925dbc32d8c167cafd5495f4c8c4d1
y-amila-nirmal <yamilanirmal@gmail.com>
2025-01-21 23:22:15 +0530

    chore: reduced app window size

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "ping",
-		Width:  1024,
-		Height: 768,
+		Width:  400,
+		Height: 400,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
